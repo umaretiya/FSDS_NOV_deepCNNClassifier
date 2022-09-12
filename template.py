737ed2d35ce0df5beebe48641e501c82ad740f2f
umaretiya <umaretiya@gmail.com>
2022-09-12 23:19:26 +0530

    basic skeleton

--- template.py
+++ template.py
@@ -1,1 +1,44 @@
 import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s :')
+
+package_name = "deepClassfier"
+
+list_of_files = [
+    ".github/workflows/.geetkeep",
+    f"src{package_name}/__init__.py",
+    f"src{package_name}/components/__init__.py",
+    f"src{package_name}/utils/__init__.py",
+    f"src{package_name}/config/__init__.py",
+    f"src{package_name}/pipeline/__init__.py",
+    f"src{package_name}/entity/__init__.py",
+    f"src{package_name}/constant/__init__.py",
+    "configs/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/stage_01.ipynb",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating a dir {filedir} and fine {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file of {filename}")
+    
+    else:
+        logging.info(f"file already exist {filename}")
