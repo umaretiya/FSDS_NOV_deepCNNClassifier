7c0830cc1efdc40000eab62442335f974d1adb08
umaretiya <umaretiya@gmail.com>
2022-09-25 23:51:28 +0530

    unit test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -5,8 +5,8 @@ from box import ConfigBox
 from ensure.main import EnsureError
 
 yaml_file = [
-    "tests/data/demo.yaml",
     "tests/data/empty.yaml",
+    "tests/data/demo.yaml",
 ]
 
 def test_read_yaml_empty():
@@ -24,26 +24,22 @@ def test_read_yaml_bad_type(path_to_yaml):
     with pytest.raises(EnsureError):
         read_yaml(path_to_yaml)
         
-
 class Test_read_yaml:
-    yaml_file = [
+    yaml_files = [
         "tests/data/empty.yaml",
-        "tests/data/demo.yaml",
+        "tests/data/demo.yaml"
     ]
 
     def test_read_yaml_empty(self):
         with pytest.raises(ValueError):
-            read_yaml(Path(self.yaml_file[0]))
-            
+            read_yaml(Path(self.yaml_files[0]))
+
     def test_read_yaml_return_type(self):
-        respone = read_yaml(Path(self.yaml_file[-1]))
+        respone = read_yaml(Path(self.yaml_files[-1]))
         assert isinstance(respone, ConfigBox)
-      
-    @pytest.mark.parametrize("path_to_yaml",yaml_file) 
+
+    @pytest.mark.parametrize("path_to_yaml", yaml_files)
     def test_read_yaml_bad_type(self, path_to_yaml):
         with pytest.raises(EnsureError):
             read_yaml(path_to_yaml)
-        
-    
-        
         
